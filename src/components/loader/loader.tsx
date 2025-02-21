dd83a09639611e9c871f361ecdb5f5744c1bb16b
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2025-02-21 11:22:54 +0300

    fix typing

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,6 +1,6 @@
 import loaderStyles from "./loader.module.css";
 
-export default function Loader({ title }: { title: string }) {
+export default function Loader({ title }: { title?: string }) {
   return (
     <div className={loaderStyles.loader}>
       {title && <h1 className="text text_type_main-medium">{title}</h1>}
