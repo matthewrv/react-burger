5b6d71b4dae69b34a6e1817a927d3fab2d342fff
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2025-04-09 10:40:59 +0300

    Extract common selectors and remove domain for intercept

--- cypress/e2e/burger-constructor/constructor.cy.ts
+++ cypress/e2e/burger-constructor/constructor.cy.ts
@@ -1,11 +1,22 @@
 /// <reference types="cypress" />
 
 describe("burger constructor page", () => {
+  const mainIngredientSelector = "[data-testid=ingredients-item-main]";
+  const bunIngredientSelector = "[data-testid=ingredients-item-bun]";
+  const sauseIngredientSelector = "[data-testid=ingredients-item-sauce]";
+  const ingredientTitleSelector = "[data-testid=ingredient-title]";
+
+  const constructorItemSelector = "[data-testid=constructor-item]";
+
+  const modalSelector = "[data-testid=modal]";
+  const modalOverlaySelector = "[data-testid=modal-overlay]";
+  const modalCloseButtonSelector = "[data-testid=modal-close-button]";
+
   beforeEach(() => {
-    cy.intercept("GET", "https://norma.nomoreparties.space/api/auth/user", {
+    cy.intercept("GET", "api/auth/user", {
       fixture: "user",
     });
-    cy.intercept("GET", "https://norma.nomoreparties.space/api/ingredients", {
+    cy.intercept("GET", "api/ingredients", {
       fixture: "ingredients",
     });
     cy.visit("http://localhost:5173", {
@@ -17,124 +28,122 @@ describe("burger constructor page", () => {
   });
 
   it("drag bun", () => {
-    cy.get("[data-testid=ingredients-item-bun]").first().as("ingredient-card");
+    cy.get(bunIngredientSelector).first().as("ingredient-card");
 
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Булка #1");
     cy.get("@ingredient-card").trigger("dragstart");
     cy.get("@drop-region")
       .trigger("drop")
-      .find("[data-testid=constructor-item]")
+      .find(constructorItemSelector)
       .should("have.length", 2);
   });
 
   it("drag ingredient", () => {
-    cy.get("[data-testid=ingredients-item-main]").first().as("ingredient-card");
+    cy.get(mainIngredientSelector).first().as("ingredient-card");
 
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Котлета");
     cy.get("@ingredient-card").trigger("dragstart");
     cy.get("@drop-region")
       .trigger("drop")
-      .find("[data-testid=constructor-item]")
+      .find(constructorItemSelector)
       .should("have.length", 1);
   });
 
   it("drag multiple instances of ingredient", () => {
-    cy.get("[data-testid=ingredients-item-main]").first().as("ingredient-card");
+    cy.get(mainIngredientSelector).first().as("ingredient-card");
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Котлета");
 
     // drag first element
     cy.get("@ingredient-card").trigger("dragstart");
     cy.get("@drop-region")
       .trigger("drop")
-      .find("[data-testid=constructor-item]")
+      .find(constructorItemSelector)
       .should("have.length", 1);
 
     // drag second element
     cy.get("@ingredient-card").trigger("dragstart");
     cy.get("@drop-region")
       .trigger("drop")
-      .find("[data-testid=constructor-item]")
+      .find(constructorItemSelector)
       .should("have.length", 2);
   });
 
   it("show modal", () => {
-    cy.get("[data-testid=ingredients-item-main]").first().as("ingredient-card");
+    cy.get(mainIngredientSelector).first().as("ingredient-card");
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Котлета")
       .click();
 
-    cy.get("[data-testid=modal]").as("modal").should("exist");
-    cy.get("[data-testid=modal-overlay]").should("exist");
+    cy.get(modalSelector).as("modal").should("exist");
+    cy.get(modalOverlaySelector).should("exist");
     cy.get("@modal").find("[data-testid=ingredient-image]").should("exist");
     cy.get("@modal").find("[data-testid=ingredient-name]").should("exist");
   });
 
   it("close modal by click on overlay", () => {
-    cy.get("[data-testid=ingredients-item-main]").first().as("ingredient-card");
+    cy.get(mainIngredientSelector).first().as("ingredient-card");
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Котлета")
       .click();
 
-    cy.get("[data-testid=modal-overlay]")
+    cy.get(modalOverlaySelector)
       .should("exist")
       .click({ force: true })
       .should("not.exist");
   });
 
   it("close modal by click on close button", () => {
-    cy.get("[data-testid=ingredients-item-main]").first().as("ingredient-card");
+    cy.get(mainIngredientSelector).first().as("ingredient-card");
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Котлета")
       .click();
 
-    cy.get("[data-testid=modal-close-button]")
+    cy.get(modalCloseButtonSelector)
       .should("exist")
       .click({ force: true })
       .should("not.exist");
   });
 
   it("create order", () => {
-    cy.get("[data-testid=ingredients-item-bun]").first().as("ingredient-card");
+    cy.get(bunIngredientSelector).first().as("ingredient-card");
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Булка #1")
       .trigger("dragstart");
     cy.get("@drop-region").trigger("drop");
 
-    cy.get("[data-testid=ingredients-item-main]").first().as("ingredient-card");
+    cy.get(mainIngredientSelector).first().as("ingredient-card");
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Котлета")
       .trigger("dragstart");
     cy.get("@drop-region").trigger("drop");
 
-    cy.get("[data-testid=ingredients-item-sauce]")
-      .first()
-      .as("ingredient-card");
+    cy.get(sauseIngredientSelector).first().as("ingredient-card");
     cy.get("@ingredient-card")
-      .find("[data-testid=ingredient-title]")
+      .find(ingredientTitleSelector)
       .should("have.text", "Соус")
       .trigger("dragstart");
     cy.get("@drop-region").trigger("drop");
 
     cy.get("[data-testid=total-price]").should("have.text", "170 ");
 
-    cy.intercept("POST", "https://norma.nomoreparties.space/api/orders", {
+    cy.intercept("POST", "api/orders", {
       fixture: "create-order",
     });
     cy.get("[data-testid=checkout-button]").click();
-    cy.get("[data-testid=modal]").should("exist");
+    cy.get(modalSelector).should("exist");
 
-    cy.get("[data-testid=modal-close-button]")
+    cy.get(modalCloseButtonSelector)
       .should("exist")
       .click({ force: true })
       .should("not.exist");
